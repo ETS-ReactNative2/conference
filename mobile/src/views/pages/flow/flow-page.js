8acb38a6524620a5bce017e41a87540d28b5abfe
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-07-09 13:57:42 +0200

    WIP

--- mobile/src/views/pages/flow/flow-page.js
+++ mobile/src/views/pages/flow/flow-page.js
@@ -22,34 +22,48 @@ import {
 } from './steps'
 
 class FlowPage extends React.Component {
+
+  componentDidMount () {
+    this.CurrentStep = <CommonProfileOnboarding/>
+  }
+
   render () {
+    const { CurrentStep } = this
+
+    console.log(CurrentStep)
+
     return (
       <Container>
         <Content>
-          <Text>Employee steps</Text>
-          <EmployeeKeywords onFill={console.log} style={ { marginBottom: 16, marginTop: 16 } }/>
-          <EmployeeRole onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <Text>Investee steps</Text>
+          <CurrentStep
+            onFill={console.log}
+            style={ { marginBottom: 16, marginTop: 16 } }
+          />
 
-          <InvesteeHiring onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvesteeIco onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvesteeMoneySource onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvesteeTeamMembers onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvesteeFundingStage onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvesteeProductStage onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvesteeLinks onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvesteeProjectSetup onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
+          {/*<Text>Employee steps</Text>*/}
+          {/*<EmployeeKeywords onFill={console.log} style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<EmployeeRole onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<Text>Investee steps</Text>*/}
 
-          <Text style={ { marginBottom: 16, marginTop: 16 } }>Common LAYOUTS:</Text>
-          <CommonProfileOnboarding onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <CommonProfileType onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
+          {/*<InvesteeHiring onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvesteeIco onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvesteeMoneySource onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvesteeTeamMembers onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvesteeFundingStage onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvesteeProductStage onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvesteeLinks onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvesteeProjectSetup onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
 
-          <Text style={ { marginBottom: 16, marginTop: 16 } }>Investor LAYOUTS:</Text>
-          <InvestorCompanyLocation onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvestorInvestIn onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvestorTicketSize onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvestorCompanyFundingStage onFill= { console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
-          <InvestorCompaniesLocation onFill= { console.log } style={ { marginBottom: 16, marginTop: 16 } }/>
+          {/*<Text style={ { marginBottom: 16, marginTop: 16 } }>Common LAYOUTS:</Text>*/}
+          {/*<CommonProfileOnboarding onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<CommonProfileType onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+
+          {/*<Text style={ { marginBottom: 16, marginTop: 16 } }>Investor LAYOUTS:</Text>*/}
+          {/*<InvestorCompanyLocation onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvestorInvestIn onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvestorTicketSize onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvestorCompanyFundingStage onFill= { console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
+          {/*<InvestorCompaniesLocation onFill= { console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/}
 
         </Content>
       </Container>
