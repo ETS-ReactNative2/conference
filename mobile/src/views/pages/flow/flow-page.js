f83874cff6d961befa314a9a52f93b68bfb4dcda
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-07-09 14:34:28 +0200

    Remove unused components from layout

--- mobile/src/views/pages/flow/flow-page.js
+++ mobile/src/views/pages/flow/flow-page.js
@@ -29,32 +29,6 @@ class FlowPage extends React.Component {
           <CurrentStep
             onFill={ this.onFill }
             style={ { marginBottom: 16, marginTop: 16 } }/>
-
-          { /*<Text>Employee steps</Text>*/ }
-          { /*<EmployeeKeywords onFill={console.log} style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<EmployeeRole onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<Text>Investee steps</Text>*/ }
-
-          { /*<InvesteeHiring onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvesteeIco onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvesteeMoneySource onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvesteeTeamMembers onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvesteeFundingStage onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvesteeProductStage onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvesteeLinks onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvesteeProjectSetup onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-
-          { /*<Text style={ { marginBottom: 16, marginTop: 16 } }>Common LAYOUTS:</Text>*/ }
-          { /*<CommonProfileOnboarding onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<CommonProfileType onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-
-          { /*<Text style={ { marginBottom: 16, marginTop: 16 } }>Investor LAYOUTS:</Text>*/ }
-          { /*<InvestorCompanyLocation onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvestorInvestIn onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvestorTicketSize onFill={ console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvestorCompanyFundingStage onFill= { console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-          { /*<InvestorCompaniesLocation onFill= { console.log } style={ { marginBottom: 16, marginTop: 16 } }/>*/ }
-
         </Content>
       </Container>
     )
