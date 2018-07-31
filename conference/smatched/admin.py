5d5279f2b4c27dbfd8d0a90754c25828e9fcd2c2
Kai Rathmann <k.o.rathmann@gmail.com>
2018-07-31 19:00:06 +0200

    Add SmatchedUser to Django Admin

--- conference/smatched/admin.py
+++ conference/smatched/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from users.models import SmatchedUser
 
-# Register your models here.
+
+class SmatchedUserAdmin(admin.ModelAdmin):
+    model = SmatchedUser
+
+
+admin.site.register(SmatchedUser, SmatchedUserAdmin)
