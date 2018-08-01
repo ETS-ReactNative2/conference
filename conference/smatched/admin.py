664a4cbc2962c7c19dfc7d6470782a59c1c76915
Kai Rathmann <k.o.rathmann@gmail.com>
2018-08-01 10:56:09 +0200

    Add ChatfuelattributesAnswer and Status to Django Admin

--- conference/smatched/admin.py
+++ conference/smatched/admin.py
@@ -2,11 +2,24 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+
+from chatfuelattributes.models import ChatfuelattributesAnswer
+from twitter.models import Status
 from users.models import SmatchedUser
+
+
+class ChatfuelattributesAnswerAdmin(admin.ModelAdmin):
+    model = ChatfuelattributesAnswer
 
 
 class SmatchedUserAdmin(admin.ModelAdmin):
     model = SmatchedUser
 
 
+class StatusAdmin(admin.ModelAdmin):
+    model = Status
+
+
+admin.site.register(ChatfuelattributesAnswer, ChatfuelattributesAnswerAdmin)
 admin.site.register(SmatchedUser, SmatchedUserAdmin)
+admin.site.register(Status, StatusAdmin)
