c95245c4d622f2102b429bde1aecddd952b13e40
Kai Rathmann <k.o.rathmann@gmail.com>
2018-08-09 14:34:17 +0200

    Feature/#184 phraseapp (#223)
    * Change local format to JSON
    
    * Fix JSON errors, add .phraseapp.yaml

--- mobile/locales/i18n.js
+++ mobile/locales/i18n.js
@@ -1,8 +1,8 @@
 import Languages from 'react-native-languages'
 import I18n from 'react-native-i18n';
-import de from './de';
-import en from './en';
-import pl from './pl';
+import de from './de.json';
+import en from './en.json';
+import pl from './pl.json';
 
 I18n.locale = Languages.language
 I18n.fallbacks = true;
