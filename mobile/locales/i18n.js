69042465313795ad9a540b3429fe8bd878f0d6ae
Kai Rathmann <k.o.rathmann@gmail.com>
2018-08-13 18:43:30 +0200

    Feature/#238 remove de pl locales (#240)

--- mobile/locales/i18n.js
+++ mobile/locales/i18n.js
@@ -1,15 +1,11 @@
 import Languages from 'react-native-languages'
 import I18n from 'react-native-i18n';
-import de from './de.json';
 import en from './en.json';
-import pl from './pl.json';
 
 I18n.locale = Languages.language
 I18n.fallbacks = true;
 I18n.translations = {
-  de,
-  en,
-  pl
+  en
 };
 
 Languages.addEventListener('change', ({ language }) => {
