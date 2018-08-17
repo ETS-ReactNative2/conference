bb3b330071b432d83919c705836ab8cd7481276c
Brian Raszap Skorbiansky <brianraszap@gmail.com>
2018-08-17 02:11:24 -0700

    Prod setup with Elastic Beanstalk (#267) (#268)

--- conference/symposion2016/wsgi.py
+++ conference/symposion2016/wsgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion2016.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
