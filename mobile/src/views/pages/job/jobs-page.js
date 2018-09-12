f4a795b99c1152d358ce5da3ec42812d3982ae84
andrzej_sz <andrzej.szmajnta.dev@gmail.com>
2018-09-12 16:46:45 +0200

    Fix/job editing (#537)

--- mobile/src/views/pages/job/jobs-page.js
+++ mobile/src/views/pages/job/jobs-page.js
@@ -41,13 +41,14 @@ export default class JobsPage extends Component {
     const project = this.props.navigation.getParam('project')
     const jobs = project.jobListings.map(jb => {
       const { id, imageUrl, name } = project
+      console.log({jb, id, imageUrl, name})
       return {
         ...jb,
         project: {
           ...jb.project,
-          id: id || jb.project.id,
-          imageUrl: imageUrl || jb.project.imageUrl,
-          name: name || jb.project.name
+          id: id !== undefined ? id :  jb.project.id,
+          imageUrl: imageUrl !== undefined ? imageUrl : jb.project.imageUrl,
+          name: name !== undefined ? name : jb.project.name
         }
       }
     })
